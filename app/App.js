ac35dd77781df58c740dcfd29d1205881ad3a41a
Ahmed Amine Doudech <hmidetocraft@gmail.com>
2022-06-23 20:12:18 +0100

    INIT

---
+++ app/App.js
@@ -0,0 +1,15 @@
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>Hello, haithem!</Text>
+    </SafeAreaView>
+  );
+}
